9fb3cbabc9d08f262449db2b305ca3c25f42165c
AspyRain <1436582605@qq.com>
2025-03-29 04:21:46 +0800

    Update globals.c

--- Keil/fish_terminal/Core/Src/globals.c
+++ Keil/fish_terminal/Core/Src/globals.c
@@ -1,8 +1,8 @@
 // globals.c
 #include "global.h"
 
-char usart1_rx_buffer[BUFFER_SIZE];
-int usart1_rx_index = 0;
+char usart2_rx_buffer[BUFFER_SIZE];
+int usart2_rx_index = 0;
 int esp_flag = 0;
 int timeout_flag = 0;
 int esp_command_flag=0;
@@ -10,3 +10,4 @@ int AT_OK_Flag = 0;
 int AT_Ready_Flag = 0;
 int AT_Connect_Net_Flag = 0;
 char *status = "";
+int checkReciveFlag = 0;
