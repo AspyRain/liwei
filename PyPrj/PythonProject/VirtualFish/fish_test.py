86032bc1f9b06d9d2a56102b19c4ed522e1f1bdf
AspyRain <1436582605@qq.com>
2025-04-04 18:06:35 +0800

    1

--- PyPrj/PythonProject/VirtualFish/fish_test.py
+++ PyPrj/PythonProject/VirtualFish/fish_test.py
@@ -1,32 +1,4 @@
-import cv2
+import torch
 
-def main(video_source=0):
-    # 打开摄像头
-    cap = cv2.VideoCapture(video_source)
-
-    # 检查摄像头是否成功打开
-    if not cap.isOpened():
-        print("无法打开摄像头")
-        return
-
-    while True:
-        # 捕获每一帧
-        ret, frame = cap.read()
-
-        if not ret:
-            print("无法读取帧")
-            break
-
-        # 显示捕获的帧
-        cv2.imshow("摄像头", frame)
-
-        # 如果按下 'q' 键，退出循环
-        if cv2.waitKey(1) & 0xFF == ord('q'):
-            break
-
-    # 释放摄像头并关闭所有窗口
-    cap.release()
-    cv2.destroyAllWindows()
-
-# 调用函数
-main(video_source=0)
+# 清除 torch 缓存
+torch.hub._remove_all_cache()
